a26a4822b929dba9d5bdaaea40ec97d21e742f49
Hovhannes <hovoqoch@gmail.com>
2021-04-23 14:08:22 +0400

    Customer service

--- resources/views/admin/members/customer-service.blade.php
+++ resources/views/admin/members/customer-service.blade.php
@@ -8,42 +8,42 @@
             </div>
             <div class="card-body">
                 <div class="row">
-                    @if($justJoined->total())
-                        <div class="col-md-6">
-                            <h4 class="text-center">Members that joined in the current week</h4>
-                            <table class="display table table-striped table-bordered no-footer" role="grid">
-                                <thead>
-                                <tr role="row">
-                                    <th>ID</th>
-                                    <th>Name</th>
-                                    <th>Company</th>
-                                    <th>Registered</th>
-                                    <th>Actions</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($justJoined as $member)
-                                    <tr role="row">
-                                        <td>{{ $member->id }}</td>
-                                        <td>{{ $member->name }}</td>
-                                        <td>{{ $member->company->name ?? '' }}</td>
-                                        <td>{{ $member->created_at }}</td>
-                                        <td>
-                                            <div class="action"><a
-                                                    href="{{ route('admin.members.profile', $member->id) }}"
-                                                    class="btn btn-sm btn-primary">Show</a></div>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                            <div class="float-left">
-                                {{ $justJoined->links() }}
-                            </div>
-                        </div>
-                    @endif
+{{--                    @if($justJoined->total())--}}
+{{--                        <div class="col-md-6">--}}
+{{--                            <h4 class="text-center">Members that joined in the current week</h4>--}}
+{{--                            <table class="display table table-striped table-bordered no-footer" role="grid">--}}
+{{--                                <thead>--}}
+{{--                                <tr role="row">--}}
+{{--                                    <th>ID</th>--}}
+{{--                                    <th>Name</th>--}}
+{{--                                    <th>Company</th>--}}
+{{--                                    <th>Registered</th>--}}
+{{--                                    <th>Actions</th>--}}
+{{--                                </tr>--}}
+{{--                                </thead>--}}
+{{--                                <tbody>--}}
+{{--                                @foreach($justJoined as $member)--}}
+{{--                                    <tr role="row">--}}
+{{--                                        <td>{{ $member->id }}</td>--}}
+{{--                                        <td>{{ $member->name }}</td>--}}
+{{--                                        <td>{{ $member->company->name ?? '' }}</td>--}}
+{{--                                        <td>{{ $member->created_at }}</td>--}}
+{{--                                        <td>--}}
+{{--                                            <div class="action"><a--}}
+{{--                                                    href="{{ route('admin.members.profile', $member->id) }}"--}}
+{{--                                                    class="btn btn-sm btn-primary">Show</a></div>--}}
+{{--                                        </td>--}}
+{{--                                    </tr>--}}
+{{--                                @endforeach--}}
+{{--                                </tbody>--}}
+{{--                            </table>--}}
+{{--                            <div class="float-left">--}}
+{{--                                {{ $justJoined->links() }}--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    @endif--}}
                     @if($firstOrders->total())
-                        <div class="col-md-6">
+                        <div class="col-md-12">
                             <h4 class="text-center">Members that have a first order coming up the current week</h4>
                             <div class="table-responsive">
                                 <table class="display table table-striped table-bordered no-footer" role="grid">
@@ -52,6 +52,7 @@
                                         <th>ID</th>
                                         <th>Name</th>
                                         <th>Company</th>
+                                        <th>Time</th>
                                         <th>Registered</th>
                                         <th>Actions</th>
                                     </tr>
@@ -62,6 +63,7 @@
                                             <td>{{ $member->id }}</td>
                                             <td>{{ $member->name }}</td>
                                             <td>{{ $member->company->name ?? '' }}</td>
+                                            <td>{{$member->bookings()->orderBy('start_date')->first()->start_date}}</td>
                                             <td>{{ $member->created_at }}</td>
                                             <td>
                                                 <div class="action"><a
